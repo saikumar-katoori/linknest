0958cfa9855ebc8679a498dfe6255ed48586acbe
saikumar-katoori <saikumarkatoori@gmail.com>
2026-03-12 06:39:28 +0530

    Updating Frontend Routing for Deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: API,
 });
 
 // Attach JWT token to every request
