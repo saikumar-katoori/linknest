a91924d6e29829993b2ed6374ed7e828e999d744
saikumar-katoori <saikumarkatoori@gmail.com>
2026-03-12 07:04:46 +0530

    Updating cors

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({origin: "*"}));
 app.use(express.json({ limit: "1mb" }));
+
+import cors from "cors";
+
 
 // Routes
 app.use("/api/auth", authRoutes);
